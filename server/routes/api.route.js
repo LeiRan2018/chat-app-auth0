381eec74eae867df49fbcf6b400924ca7443c173
leiran2018 <jackran410@gmail.com>
2019-01-30 15:08:06 -0500

    made some changes

--- server/routes/api.route.js
+++ server/routes/api.route.js
@@ -4,9 +4,11 @@ var router = express.Router()
 var chat = require('./api/chat.route');
 var login = require('./api/login.route');
 var sign = require('./api/sign.route');
+var one = require('./api/one.route');
 
 router.use('/chat', chat);
 router.use('/login', login);
 router.use('/sign', sign);
+router.use('/one', one);
 
 module.exports = router;
--- server/routes/api/chat.route.js
+++ server/routes/api/chat.route.js
@@ -3,7 +3,5 @@ var router = express.Router();
 
 var chatController = require('../../controllers/chat.controller');
 
-router.post('/post', chatController.postchat);
-router.post('/login', chatController.postuser);
 router.post('/postchat', chatController.postchat);
 module.exports = router;
